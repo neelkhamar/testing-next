59faee2faf9430ab95d47df349608136a8560872
Neelcd <neelpatel0806@gmail.com>
2022-10-18 02:01:02 +0530

    Fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  console.log("Environement Variables", process.env);
+  console.log("Environement Variables", process.env.Environment);
   return <div className={styles.container}></div>;
 }
