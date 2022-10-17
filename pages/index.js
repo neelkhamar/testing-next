9923106c036e745c3557238e2078919ee99e9b1d
Neelcd <neelpatel0806@gmail.com>
2022-10-18 02:10:21 +0530

    Change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   console.log(
     "Environement Variables",
     process.env.NEXT_PUBLIC_VERCEL_Environment,
-    VERCEL_ENV
+    process.env.VERCEL_ENV
   );
   return <div className={styles.container}></div>;
 }
