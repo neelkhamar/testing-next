7f5004c781a1c5d050a991f615aa53ccc3fe1492
Neelcd <neelpatel0806@gmail.com>
2022-10-18 02:08:33 +0530

    C

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   console.log(
     "Environement Variables",
-    process.env.NEXT_PUBLIC_VERCEL_Environment
+    process.env.NEXT_PUBLIC_VERCEL_Environment,
+    VERCEL_ENV
   );
   return <div className={styles.container}></div>;
 }
