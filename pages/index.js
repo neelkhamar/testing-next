6de87eff323bf07809aaf27134db9e6065bb37d0
Neelcd <neelpatel0806@gmail.com>
2022-10-18 02:06:43 +0530

    C

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  console.log("Environement Variables", process.env.Environment);
+  console.log(
+    "Environement Variables",
+    process.env.NEXT_PUBLIC_VERCEL_Environment
+  );
   return <div className={styles.container}></div>;
 }
